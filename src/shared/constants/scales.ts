d947c3dbe1f396e4874869e36e78d1b4d6a08698
zeriong <jaeryong95@gmail.com>
2025-12-18 23:39:53 +0900

    refactor: 공용 스케일, 메시지 추가

---
+++ src/shared/constants/scales.ts
@@ -0,0 +1,17 @@
+export const MODEL_ROOM_OBJECT_POSITIONS = {
+  DOLL: { x: 0, y: 100, z: -150 },
+  MONITOR: { x: 68.2, y: 171.6, z: -174.8 },
+  POSTER: { x: -113.8, y: 231.2, z: -182.1 },
+  FRAME1: { x: 22.6, y: 245.2, z: -187.6 }, // { x: 1.226, y: 2.451, z: 0.123 },
+  FRAME2: { x: 91.8, y: 245.2, z: -187.6 }, // { x: 1.918, y: 2.451, z: 0.123 },
+  GUESTBOOK_AREA: { x: -50, y: 200, z: 19.4 },
+};
+
+export const MODEL_ROOM_OBJECT_SIZES = {
+  MONITOR: { w: 876, h: 547 },
+  POSTER: { w: 986, h: 1500 },
+  FRAME1: { w: 520, h: 745 },
+  FRAME2: { w: 554, h: 477 },
+};
+
+export const MODEL_ROOM_OBJECT_SCALE = new Array(3).fill(0.1);
